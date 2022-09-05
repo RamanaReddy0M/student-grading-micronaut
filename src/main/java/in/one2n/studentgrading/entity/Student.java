fea15f2f3b408fb57fed2b641b9dbcf14afe1237
Ramana Reddy <90540245+RamanaReddy0M@users.noreply.github.com>
2022-09-05 17:41:19 +0530

    - Configure psotgres through docker-compose (#1)
    - setup connection to postgres database by adding dependencies:
    	- postgresql, micronaut-jdbc-hikari, micronaut-data-hibernate-jpa

--- src/main/java/in/one2n/studentgrading/entity/Student.java
+++ src/main/java/in/one2n/studentgrading/entity/Student.java
@@ -1,5 +1,47 @@
 package in.one2n.studentgrading.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
+import io.micronaut.core.annotation.Introspected;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "students")
+@Introspected
 public class Student {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  private String firstName;
+
+  private String lastName;
+
+  private String university;
+
+  private Double test1Score;
+
+  private Double test2Score;
+
+  private Double test3Score;
+
+  private Double test4Score;
+
+  @Transient
+  private Double finalScore;
+
+  @Transient
+  private Grade grade;
 }
