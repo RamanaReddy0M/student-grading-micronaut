c9eb1bef442bc43db21ec42c03cc268a5fb23018
Ramana Reddy <ramanareddy.manda@practo.com>
2022-09-06 17:37:51 +0530

    - Add test cases for university-wise-topper and overall-topper

--- src/test/java/in/one2n/studentgrading/controller/StudentControllerTest.java
+++ src/test/java/in/one2n/studentgrading/controller/StudentControllerTest.java
@@ -16,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -36,15 +37,14 @@ class StudentControllerTest {
 
   @Test
   void testItWorks() {
-    System.out.println(embeddedServer.getHost() + ", " + embeddedServer.getPort());
     assertTrue(embeddedServer.isRunning());
   }
 
   @Test
+  @Order(1)
   void getStudentById() throws InterruptedException {
     //when:
     HttpRequest<Object> request = HttpRequest.GET("/v1/api/student?id=1");
-
     HttpResponse<Student> response = httpClient.toBlocking().exchange(request, Student.class);
 
     //then:
@@ -58,23 +58,20 @@ class StudentControllerTest {
     assertNotNull(student);
     assertEquals("Kaylen", student.getFirstName());
 
-    Thread.sleep((1000 * 60 * 3));
+    //when: student
+    request = HttpRequest.GET("/v1/api/student?id=10001");
+    HttpResponse<Object> response2 = httpClient.toBlocking().exchange(request, Object.class);
+
+    // then:
+    assertNotNull(response2);
   }
 
   @Test
-  void addStudent() {
-  }
-
-  @Test
-  void updateStudent() {
-  }
-
-  @Test
-  void getAllStudents() throws InterruptedException {
+  @Order(2)
+  void getAllStudents() {
 
     //when:
     HttpRequest<Object> request = HttpRequest.GET("/v1/api/student/all");
-
     HttpResponse<List<Student>> response =
         httpClient.toBlocking().exchange(request, Argument.listOf(Student.class));
 
@@ -92,6 +89,82 @@ class StudentControllerTest {
   }
 
   @Test
+  @Order(3)
+  void getUniversityWiseTopper() {
+    //when:
+    List<Student> topperListFromController = httpClient.toBlocking()
+        .retrieve(HttpRequest.GET("/v1/api/student/university-wise-topper"),
+            Argument.listOf(Student.class));
+
+    //then:
+    assertNotNull(topperListFromController);
+    assertEquals("Izayah", topperListFromController.get(0).getFirstName());
+    assertEquals("University of California", topperListFromController.get(1).getUniversity());
+  }
+
+  @Test
+  @Order(4)
+  void getOverallTopperTest() {
+    //when:
+    String requestBody =
+        "{" + "\"firstName\": \"Pratham\",\n"
+            + "\"lastName\": \"Ajmire\",\n" +
+            "\"university\": \"RCOEM\",\n" +
+            "\"test1Score\": 100.0,\n" +
+            "\"test2Score\": 100.0,\n" +
+            "\"test3Score\": 99.0,\n" +
+            "\"test4Score\": 99.0 }";
+    HttpRequest<Object> request = HttpRequest.POST("/v1/api/student/create", requestBody);
+    HttpResponse<Student> response = httpClient.toBlocking().exchange(request, Student.class);
+    Student student = response.body();
+
+    //then:
+    assertNotNull(student);
+    assertEquals("Ajmire", student.getLastName());
+
+    //when:
+    List<Student> topperList = httpClient.toBlocking()
+        .retrieve(HttpRequest.GET("/v1/api/student/topper"), Argument.listOf(Student.class));
+
+    //then:
+    assertNotNull(topperList);
+    assertEquals("Pratham", topperList.get(0).getFirstName());
+  }
+
+  @Test
+  @Order(5)
+  void updateStudent() {
+  }
+
+  @Test
+  @Order(6)
+  void addStudent() {
+    //when:
+    String requestBody =
+        "{" + "\"firstName\": \"Ragnar\",\n" +
+            "\"lastName\": \"Lothruk\",\n" +
+            "\"university\": \"Duke University\",\n" +
+            "\"test1Score\": 99.0,\n" +
+            "\"test2Score\": 99.0,\n" +
+            "\"test3Score\": 99.0,\n" +
+            "\"test4Score\": 90.0 }";
+    HttpRequest<Object> request = HttpRequest.POST("/v1/api/student/create", requestBody);
+    HttpResponse<Student> response = httpClient.toBlocking().exchange(request, Student.class);
+
+    //then:
+    assertNotNull(response);
+    assertEquals(HttpStatus.OK, response.getStatus());
+
+    //when:
+    Student student = response.body();
+
+    //then:
+    assertNotNull(student);
+    assertEquals("Ragnar", student.getFirstName());
+  }
+
+  @Test
+  @Order(7)
   void deleteById() {
   }
 }
